c2d11ab91c5a6192fec07e9d871d1ac61bcb2508
saifali509 <saifalipasha002@gmail.com>
2026-02-06 17:58:45 +0530

    Initial commit counter-app

---
+++ app.js
@@ -0,0 +1,20 @@
+let count = 0;
+const countDisplay = document.querySelector("#count");
+const increaseBtn = document.querySelector("#increase");
+const decreaseBtn = document.querySelector("#decrease");
+const resetBtn = document.querySelector("#reset");
+
+increaseBtn.addEventListener("click",function(){
+    count++;
+    countDisplay.textContent = count;
+});
+
+decreaseBtn.addEventListener("click",function(){
+    count--;
+    countDisplay.textContent = count;
+});
+
+resetBtn.addEventListener("click",function(){
+    count = 0;
+    countDisplay.textContent = count;
+});
